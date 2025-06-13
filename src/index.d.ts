577f9bd5722acccfada3e5974820a667c7f7dad2
royhansen99 <roy@royhansen.no>
2025-06-13 15:06:08 +0200

    feat: Add typescript typings.

---
+++ src/index.d.ts
@@ -0,0 +1,26 @@
+declare module 'routes' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    type CallbackFunction = (...args: any[]) => any;
+
+    const Router: () => {
+        routes: {
+            re: RegExp;
+            src: string;
+            keys: string[];
+            fn: CallbackFunction;
+        }[];
+        routeMap: { [key: string | RegExp]: CallbackFunction };
+        addRoute: (path: string | RegExp, fn: CallbackFunction) => void;
+        removeRoute: (path: string | RegExp) => void;
+        // match: () => { re: RegExp, src: string, keys: string[] };
+        match: (pathname: string, startAt?: number) => {
+            params: { [key: string]: string };
+            splats: string[];
+            route: string;
+            fn: CallbackFunction;
+            next: () => void;
+        };
+    };
+
+    export default Router;
+}
