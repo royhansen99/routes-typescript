690928e017fd7756b9d8c722238b821effa04d5b
royhansen99 <roy@royhansen.no>
2025-06-16 09:57:14 +0200

    fix: Remove `module` declaration from typings, not needed and the module name was also wrong.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,29 +1,27 @@
-declare module 'routes' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  type CallbackFunction = (...args: any[]) => any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type CallbackFunction = (...args: any[]) => any
 
-  const Router: () => {
-    routes: {
-      re: RegExp
-      src: string
-      keys: string[]
-      fn: CallbackFunction
-    }[]
-    routeMap: { [key: string | RegExp]: CallbackFunction }
-    addRoute: (path: string | RegExp, fn: CallbackFunction) => void
-    removeRoute: (path: string | RegExp) => void
-    // match: () => { re: RegExp, src: string, keys: string[] };
-    match: (
-      pathname: string,
-      startAt?: number
-    ) => {
-      params: { [key: string]: string }
-      splats: string[]
-      route: string
-      fn: CallbackFunction
-      next: () => void
-    }
+declare const Router: () => {
+  routes: {
+    re: RegExp
+    src: string
+    keys: string[]
+    fn: CallbackFunction
+  }[]
+  routeMap: { [key: string | RegExp]: CallbackFunction }
+  addRoute: (path: string | RegExp, fn: CallbackFunction) => void
+  removeRoute: (path: string | RegExp) => void
+  // match: () => { re: RegExp, src: string, keys: string[] };
+  match: (
+    pathname: string,
+    startAt?: number
+  ) => {
+    params: { [key: string]: string }
+    splats: string[]
+    route: string
+    fn: CallbackFunction
+    next: () => void
   }
-
-  export default Router
 }
+
+export default Router
